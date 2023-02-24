075262869f3482cd65db993552d636e357d19d87
kipngeno kevin <kipngenokevin254@gmail.com>
2023-02-24 18:15:01 +0300

    Strapi to Aws now

--- api/config/plugins.js
+++ api/config/plugins.js
@@ -1,7 +1,24 @@
 module.exports = ({ env }) => ({
-    //...
-    'import-export-entries': {
-      enabled: true,
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AKIARCFMPRTL4V4DUERO'),
+        secretAccessKey: env('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'),
+        region: env('us-east-1'),
+        params: {
+            Bucket: env('my-strapi-project-image'),
+        },
+      },
+      // These parameters could solve issues with ACL public-read access — see [this issue](https://github.com/strapi/strapi/issues/5868) for details
+      actionOptions: {
+        upload: {
+          ACL: null
+        },
+        uploadStream: {
+          ACL: null
+        },
+      }
     },
-    //...
-  });
+  }
+});

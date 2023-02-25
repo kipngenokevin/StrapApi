3295e7a04d37b5911b937e62c0e99890724cbbac
kipngeno kevin <kipngenokevin254@gmail.com>
2023-02-25 15:58:42 +0300

    25th

--- api/config/database.js
+++ api/config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'storage'),
+      database: env('DATABASE_NAME', 'store'),
       user: env('DATABASE_USERNAME', 'root'),
       password: env('DATABASE_PASSWORD', 'Makeitsimple'),
       ssl: env.bool('DATABASE_SSL', false),
